f381048fc8d855b0c9ad72dc394a8ea1a2142ee6
aboucher <aboucher@twilio.com>
2021-06-03 20:41:57 -0700

    gitignore fix

--- node_modules/html-react-parser/index.mjs
+++
@@ -1,7 +0,0 @@
-import HTMLReactParser from './index.js';
-
-export var domToReact = HTMLReactParser.domToReact;
-export var htmlToDOM = HTMLReactParser.htmlToDOM;
-export var attributesToProps = HTMLReactParser.attributesToProps;
-
-export default HTMLReactParser;
